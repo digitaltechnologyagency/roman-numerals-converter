f7085afe08ec3980bef9b0241de5e781f02b37cf
deemak <deema_k@mail.ru>
2021-01-11 11:25:00 +0300

    first

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# RomanNumeralsConverter
+# Roman Numerals Converter
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.24.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Subject} from 'rxjs';
+import {Logger} from './base/logger-service';
+
+const log = new Logger(`auth`);
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'roman-numerals-converter';
+export class AppComponent implements OnInit {
+    private textChange$: Subject<any> = new Subject<any>();
+    romanNumbers: string[] = ['M', 'XM', 'CM', 'D', 'XD', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    arabNumbers: number[] = [1000, 990, 900, 500, 490, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    result: any = '';
+
+    ngOnInit(): void {
+        this.textChange$.subscribe(resp => {
+            this.toRoman(resp);
+        });
+    }
+
+    public toRoman(num: any) {
+        this.result = '';
+        let i = 0;
+        while (num > 0 || this.arabNumbers.length === (i - 1)) {
+            while ((num - this.arabNumbers[i]) >= 0) {
+                num -= this.arabNumbers[i];
+                this.result += this.romanNumbers[i];
+            }
+            i++;
+        }
+    }
 }
